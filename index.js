5c27466017ef3b255eac73314bb82c691b44429b
Mitchell Levy <levymitchell0@gmail.com>
2019-02-12 21:03:36 -0800

    Added scaffolding for multiple slides.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ document.body.appendChild(renderer.domElement);
 let currentAnimationFrame;
 
 let scene, camera;
-function i1() {
+
+let canvas = document.getElementsByTagName('canvas')[0];
+
+function drivewaySphere() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   var controls = new THREE.OrbitControls(camera);
   controls.enableZoom = false;
@@ -32,15 +35,32 @@ function render() {
   currentAnimationFrame = requestAnimationFrame(render);
 }
 
-i1();
+
+
+// TEMP:
+drivewaySphere();
 currentAnimationFrame = requestAnimationFrame(render);
 
+
+
 let currentSlide = 0;
+//Each slide is [init, bool:shouldUpdate]
+let slides = [[drivewaySphere, true]];
 
 document.addEventListener('keydown', (event) => {
-  console.log(`key!`);
   if (event.keyCode == 32) {
     cancelAnimationFrame(currentAnimationFrame);
-    console.log(`spacebar!`);
+    currentSlide++;
+    if (currentSlide < slides.length) {
+      slides[currentSlide][0]();
+
+      //If shouldUpdate
+      if (slides[currentSlide][1]) {
+        currentAnimationFrame = requestAnimationFrame(render);
+      }
+
+    } else {
+      //End.
+    }
   }
 })
