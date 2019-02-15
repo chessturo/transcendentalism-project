1590e01b3c98f00c92717a584d7540e89d1debfb
Mitchell Levy <levymitchell0@gmail.com>
2019-02-14 22:38:47 -0800

    Intro to transcendentalism done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function render() {
 }
 
 async function drivewaySphere() {
-   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   var controls = new THREE.OrbitControls(camera);
   controls.enableZoom = false;
   controls.enablePan = false;
@@ -44,12 +44,18 @@ async function drivewaySphere() {
   controls.update();
 }
 
+let textSlideOpen = "<div style='margin-left: auto; margin-right: auto; vertical-align: middle; padding: 5vw'><p style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 5vh; margin: 0;'>";
+let textSlideClose = "</p></div>";
+
+
+//This is a complete mess, and should be refactored. Unfortunately, ain't nobody got time for that.
+
 async function title() {
   div.innerHTML = " \
-  <div style='margin-left: auto; margin-right: auto; vertical-align: middle;'> \
-    <h1 style='font-family: \"Cormorant\", serif; font-weight: 300; font-style: italic; font-size: 10vh;' >Transcendentalism</h1> \
-    <h4 style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 3vh;' >By: Mitchell Levy</h4> \
-  </div> \
+    <div style='margin-left: auto; margin-right: auto; vertical-align: middle;'> \
+      <h1 style='font-family: \"Cormorant\", serif; font-weight: 300; font-style: italic; font-size: 10vh;' >Transcendentalism</h1> \
+      <h4 style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 3vh;' >By: Mitchell Levy</h4> \
+    </div> \
   ";
   div.style.cssText = "display: flex; align-items: center;";
   await sleep(50);
@@ -57,49 +63,99 @@ async function title() {
 }
 
 async function intro() {
-  div.innerHTML = " \
-  <div style='margin-left: auto; margin-right: auto; vertical-align: middle; padding: 1vw'> \
-    <p style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 2vh;' > \
-      Transcendentalism is cool...\
+  div.innerHTML = textSlideOpen + " \
+    <span style='font-size: 6vh;'>Transcendentalism</span> is, at its core, about how one <i>experiences the world.</i> \
+  " + textSlideClose;
+  div.style.cssText = "display: flex; align-items: center;";
+  await sleep(50);
+  div.classList.toggle("fade");
+}
+
+async function transcendIntro1() {
+  div.innerHTML = textSlideOpen + " \
+    Transcendentalist ideology posits that the mind creates experiences; the mind itself is a lens through which we view the world. \
+    Rather than being a blank tablet that the world molds, each individual person's experiences are shaped by innate factors. \
+    Despite this, Transcendentalism holds that divinity does exist, but is within everything: nature, the works of man, and man itself.  \
+  " + textSlideClose;
+  div.style.cssText = "display: flex; align-items: center;";
+  await sleep(50);
+  div.classList.toggle("fade");
+}
+
+async function transcendIntro2() {
+  div.innerHTML = textSlideOpen + " \
+    Much of the work of the Transcendentalists is designed to explain this relationship between the divine and man, as well as show how experiences are shaped by individuals. <br /><br /> \
+    Transcendentalism hinges upon the idea that the mind itself has some innate perspective, and this perspective acts upon how individuals experience the world, as opposed to the mind being a <i>tabula rasa</i>, or \"blank slate,\" which is impressed wholly by experiences. \
+    In other words, in the classic question of whether the mind is formed by nature or nurture, Transcendentalism firmly answers nature (Goodman). \
+  " + textSlideClose;
+  div.style.cssText = "display: flex; align-items: center;";
+  await sleep(50);
+  div.classList.toggle("fade");
+}
+
+async function transcendIntro3() {
+  div.innerHTML = textSlideOpen + " \
+    This idea is based in the work of Immanuel Kant; he called this idea the, \"Copernican Revolution in philosophy\" (Goodman). \
+    To reject the empiricist movement's idea of the tabula rasa, he used what he called <i>transcendental arguments.</i> \
+    For example, Kant rejected the tabula rasa by claiming that one can only experience the world under a framework provided by the mind. \
+    \"It must be the mind's structuring, Kant argues, that makes experience possible\" (\"Immanuel Kant: Metaphysics\"); \
+    therefore, the mind cannot be entirely based upon experiences, as having a consciousness is a prerequisite of having experiences in the first place. <br /><br /> \
+    This influence shaped much of Transcendentalist thought, specifically their ideas that truth and divinity could be uncovered through individual experience. \
+    Because experience is shaped through the mind, and consciousness itself is divine, they asserted that individual experience would help attain a connection to this universal truth and divinity. \
+  " + textSlideClose;
+  div.style.cssText = "display: flex; align-items: center;";
+  await sleep(50);
+  div.classList.toggle("fade");
+}
+
+
+async function transcendIntroDemo() {
+  div.innerHTML = textSlideOpen + " \
+    One simple example of how our minds alters our experience is looking at how our brains mask our eye's blind spot. <br /> \
+    To demonstrate: <br /> \
+    1) Hold your right thumb out at arm's length. <br /> \
+    2) Put the tip of your thumb directly beneath the dot on your screen. <br /> \
+    3) Close your left eye. <br /> \
+    4) Slowly move your arm to the right, keeping your eye on the dot. <br />  \
     </p> \
-  </div> \
-  ";
+    <div style=\"font-size: 10vh; text-align: center; margin-left: auto; margin-right: auto;\">&sdot;</div> <br /> \
+    <p style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 5vh; margin: 0;'> \
+    After you move your arm about twenty degrees off of center, you should notice that your thumb has disappeared. \
+    Notably, your mind just fills this blind spot with whatever color surrounds it, meaning that you don't often notice the blind spot at all. \
+    While this example is fairly trivial, it showcases a simple, yet very subtle way your mind falsifies information and how your mind can and does change what you experience. \
+  " + textSlideClose;
+  div.style.cssText = "display: flex; align-items: center;";
+  await sleep(50);
+  div.classList.toggle("fade");
+}
+
+async function transcendIntro4() {
+  div.innerHTML = textSlideOpen + " \
+    Returning to the Transcendentalists, it is important to note that the Transcendentalists felt that people had abandoned their connection with nature and individual experience. \
+    Indeed, Emerson wrote that, \"we are now so far from the road to truth, that religious teachers dispute and hate each other, and speculative men are esteemed unsound and frivolous\" (Emerson 215). <br /><br /> \
+    Much of Transcendentalist writing seems to focus on solving this problem, with Emerson's Nature attempting to take its reader on a journey to inquire, \"to what end is nature?\" (Emerson 215). \
+    As Transcendental philosophy hinges on the idea of individual experience, the writings produced by this movement are often experiences in themselves, rather than prescriptive guides on living life, which would run counter to many of the movement's ideals. \
+  " + textSlideClose;
   div.style.cssText = "display: flex; align-items: center;";
   await sleep(50);
   div.classList.toggle("fade");
 }
 
 async function stars() {
-  div.innerHTML = " \
-  <div style='margin-left: auto; margin-right: auto; vertical-align: middle; padding: 1vw'> \
-    <p style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 2vh;' > \
-      Transcendentalism is cool...\
-    </p> \
-  </div> \
-  ";
+  div.innerHTML = textSlideOpen + " \
+    Emerson said something about stars. That's pretty cool. \
+  " + textSlideClose;
   div.style.cssText = "display: flex; align-items: center;";
   await sleep(50);
   div.classList.toggle("fade");
 }
 
 async function credits() {
-  div.innerHTML = " \
-  <div style='margin-left: auto; margin-right: auto; vertical-align: middle; padding: 1vw'> \
-    <p style='font-family: \"Cormorant\", serif; font-weight: 300; font-size: 2vh;' > \
-      Built using the amazing <a href=\"https://threejs.org/\">Threejs</a> library <br /><br /> \
-      This fancy font is Cormorant, which can be found <a href=\"https://fonts.google.com/specimen/Cormorant\">here</a> <br /><br /> \
-      Lorem ipsum dolor sit amet, consectetur adipiscing elio. Phasellus vel erat in libero consectetur feugiat nec vel magna. Cras bibendum ante ac arcu tempor molestie. Etiam a pellentesque turpis, ut commodo sapien. Nulla tincidunt vestibulum dui, at consectetur turpis ultricies in. Nullam sollicitudin porta urna, eu tristique risus fermentum porttitor. Suspendisse diam risus, porta ac placerat eget, accumsan nec eros. Vestibulum aliquet nibh est, et ultrices turpis imperdiet eget. In  porttitor mi ac turpis dapibus aliquet. <br /><br /> \
-      \
-      Vivamus non mi est. Sed tellus quam, maximus sit amet finibus et, dignissim vel tortor. Fusce ac accumsan sapien. Curabitur tristique consequat sem a auctor. In sit amet magna at lacus faucibus pellentesque. Sed venenatis, metus facilisis maximus laoreet, diam orci eleifend ipsum, quis tempor sapien nisl vel metus. Praesent vestibulum odio ultrices, auctor tellus a, molestie quam. <br /><br /> \
-      \
-      Etiam faucibus mauris ac augue accumsan, ac porttitor dolor fringilla. Vestibulum finibus, tortor a maximus lobortis, tellus lorem auctor mauris, ac consequat risus augue et mauris. Praesent sed risus laoreet justo lacinia euismod vitae at ex. Donec sollicitudin magna et posuere efficitur. Suspendisse in convallis tortor, at sollicitudin libero. Integer gravida mi ipsum, eget cursus libero dapibus et. Nullam ligula quam, auctor nec bibendum vel, ullamcorper quis dolor. Duis facilisis, massa in ultricies convallis, turpis nisl vestibulum nisi, non cursus risus massa iaculis velit. Sed finibus, felis mattis imperdiet laoreet, ligula justo auctor sapien, eu maximus odio nulla in sapien. Morbi molestie orci nibh, in tristique magna faucibus ut. Donec sit amet lectus id orci posuere eleifend at eget urna. Suspendisse mollis euismod tincidunt. Suspendisse potenti. Proin vehicula odio nec neque mattis faucibus. Nullam sodales dolor eget ex condimentum tristique.<br /><br /> \
-      \
-      Quisque ultricies tempus dapibus. Quisque sagittis vehicula dui vel pulvinar. Nullam vestibulum suscipit purus eu gravida. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam placerat mollis gravida. Morbi euismod sem diam, et varius ligula commodo non. Cras porta ex et libero convallis, quis suscipit lacus blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat lectus vel enim dapibus feugiat. Nullam feugiat metus eu varius lobortis. Nam et maximus quam. Duis sodales justo justo, sed consequat tortor condimentum accumsan. Donec a posuere ante. <br /><br /> \
-      \
-      Vivamus hendrerit non nisl eu vulputate. Integer odio nisi, congue malesuada nisl in, tempor rhoncus est. Duis ut faucibus turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed magna ac leo ullamcorper vehicula in vel velit. Quisque vitae sollicitudin metus. Nullam elit risus, molestie ut mattis eget, porttitor nec lorem. Proin maximus efficitur eros, nec mattis velit sodales sit amet.<br /><br />  \
-    </p> \
-  </div> \
-  ";
+  div.innerHTML = textSlideOpen + " \
+    Built using the amazing <a href=\"https://threejs.org/\">Threejs</a> library <br /><br /> \
+    This fancy font is Cormorant, which can be found <a href=\"https://fonts.google.com/specimen/Cormorant\">here</a> <br /><br /> \
+    Lorem ipsum dolor sit amet, consectetur adipiscing elio. Phasellus vel erat in libero consectetur feugiat nec vel magna. Cras bibendum ante ac arcu tempor molestie. Etiam a pellentesque turpis, ut commodo sapien. Nulla tincidunt vestibulum dui, at consectetur turpis ultricies in. Nullam sollicitudin porta urna, eu tristique risus fermentum porttitor. Suspendisse diam risus, porta ac placerat eget, accumsan nec eros. Vestibulum aliquet nibh est, et ultrices turpis imperdiet eget. In porttitor mi ac turpis dapibus aliquet. <br /><br /> \
+  " + textSlideClose;
   div.style.cssText = "display: flex; align-items: center;";
   await sleep(50);
   div.classList.toggle("fade");
@@ -113,14 +169,29 @@ title();
 
 let currentSlide = 0;
 //Each slide is [init, bool:shouldUpdate]
-let slides = [[title, false], [intro, false], [stars, false], [drivewaySphere, true], [credits, false]];
+let slides = [
+  [title, false],
+  [intro, false],
+
+  [transcendIntro1, false],
+  [transcendIntro2, false],
+  [transcendIntro3, false],
+  [transcendIntroDemo, false],
+  [transcendIntro4, false],
+
+  [stars, false],
+  [drivewaySphere, true],
+  [credits, false]
+];
 
 document.addEventListener("contextmenu", function handleContextMenu(event) {
   event.preventDefault();
 });
 
 document.addEventListener('keydown', async function(event) {
+  //If the key pressed is spacebar.
   if (event.keyCode == 32) {
+
     //If it's a threejs slide
     if (slides[currentSlide][1]) {
       cancelAnimationFrame(currentAnimationFrame);
@@ -133,6 +204,7 @@ document.addEventListener('keydown', async function(event) {
 
     currentSlide++;
 
+    //If there are more slides
     if (currentSlide < slides.length) {
       slides[currentSlide][0]();
 
@@ -141,6 +213,7 @@ document.addEventListener('keydown', async function(event) {
         currentAnimationFrame = requestAnimationFrame(render);
       }
 
+    //If the presentation is finished.
     } else {
       canvas.style.display = "none";
       div.classList.toggle("fade");
